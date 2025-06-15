f63d66207879a44f83c0448321a5e3826f7a570b
Dilahk900 <khalid.idrissif@gmail.com>
2025-06-15 15:59:18 +0200

    login form

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+  let form = document.getElementById('registration-form');
+  let feedbackDiv = document.getElementById('form-feedback');
+
+  form.addEventListener("submit", function(event){
+    //Form Submission Event Listener
+    event.preventDefault();
+
+    //User Inputs
+    username = document.getElementById('username').value.trim();
+    email = document.getElementById('email').value.trim();
+    password = document.getElementById('password').value.trim();
+    
+    // Validation Variables
+    let isValid = true;
+    let message = [];
+
+    //Validations
+    if(username.length < 3){
+      isValid = false;
+      message.push('Username must be at least 3 letters !');
+    }
+
+    if(!email.includes('@') || !email.includes('.')){
+      isValid = false;
+      message.push('Please enter a valid email address !');
+    }
+
+    if(password.length < 8){
+      isValid = false;
+      message.push('Password must be at least 8 letters !');
+    }    
+
+    //Feedback Display
+    feedbackDiv.style.display = 'block';
+
+    if(isValid){
+      feedbackDiv.innerHTML = 'Registration successful!';
+      feedbackDiv.color = '#28a745';
+    } else{
+      feedbackDiv.innerHTML = message.join('<br>');
+      feedbackDiv.color = '#dc3545';
+    }
+
+  });
+});
